47e31895a95dc32ba53de6d7fad079c0dde28a3b
Shreya Shrivastava <126598622+shrivastavanolo@users.noreply.github.com>
2024-02-19 00:23:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import torch
 import helper as df
 
-img,image=df.get_img(r"C:\Users\shrey\OneDrive\Documents\sigmoid\image-processing\wall images\2.jpg")
+img,image=df.get_img(r"<path-to-image>")
 predictions=df.get_preds(image)
 img,masksd=df.mask_img(image,predictions,img)
 im_pil = df.convert_cv_img(img)
 
-pipeline=torch.load(r"C:\Users\shrey\OneDrive\Documents\sigmoid\image-processing\model.h5")
-df.inpaintf(pipeline,im_pil,masksd,"hi2.jpeg")
+pipeline=torch.load(r"path-to-model/model.h5")
+df.inpaintf(pipeline,im_pil,masked,"<image-name>")
